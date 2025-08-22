0bd5cfe57b38c47deeeceb7c953974b153554b34
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-08-22 20:35:25 +0200

    Removed next lint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,3 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
 import { defineConfig, globalIgnores } from "eslint/config";
 
 import useAgnostic, {
@@ -10,15 +6,7 @@ import useAgnostic, {
   // enforceEffectiveDirectivesRuleName
 } from "eslint-plugin-use-agnostic";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
 const eslintConfig = defineConfig([
-  // ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
