c8e17c7f97972daea6b992b022f57689e5e0482b
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-08-26 16:02:32 +0200

    Beginning work on Extra JSX (.xjsx/.xtsx).

---
+++ app/(pages)/websockets/page.x.tsx
@@ -0,0 +1,18 @@
+import { findLatestMessages } from "@/reads/messages";
+
+import WebSocketsClientPage from "./client";
+
+/** The "outer", Server part of the page. A Server Component, it accesses the server directly to retrieve the latest messages, instantiating any load of the page with the freshest data directly from the server, before it renders the page. */
+export default async function WebSocketsServerPage() {
+  // gets the latest messages from the database
+  const initialMessages = await findLatestMessages();
+  console.log("initialMessages are:", initialMessages);
+
+  // @ts-ignore: .xtsx
+  return <WebSocketsClientPage initialMessages />;
+}
+
+/* Notes
+<WebSocketsClientPage initialMessages />
+Turning this syntax into <WebSocketsClientPage initialMessages={initialMessages} /> via a TypeScript server plugin could be my next project.
+*/
