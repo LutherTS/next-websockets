f3078d40e145594be9e71400c927c9219ab7539d
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-08-26 21:47:39 +0200

    About to migrate the db for better-auth.

--- app/(pages)/websockets/page.tsx
+++ app/(pages)/websockets/page.tsx
@@ -1,5 +1,7 @@
 import { findLatestMessages } from "@/reads/messages";
 
+import { auth } from "~/better-auth/server/auth";
+
 import WebSocketsClientPage from "./client";
 
 /** The "outer", Server part of the page. A Server Component, it accesses the server directly to retrieve the latest messages, instantiating any load of the page with the freshest data directly from the server, before it renders the page. */
@@ -7,6 +9,20 @@ export default async function WebSocketsServerPage() {
   // gets the latest messages from the database
   const initialMessages = await findLatestMessages();
   console.log("initialMessages are:", initialMessages);
+
+  // testing better-auth on the server
+  const data = await auth.api.signUpEmail({
+    body: {
+      name: "John Doe", // required
+      email: "john.doe@example.com", // required
+      password: "password1234", // required
+      // image: "https://example.com/image.png",
+      // callbackURL: "https://example.com/callback",
+      username: "John_Doe",
+      displayUsername: "John Doe",
+    },
+  });
+  console.log("data", data);
 
   return <WebSocketsClientPage initialMessages={initialMessages} />;
 }
