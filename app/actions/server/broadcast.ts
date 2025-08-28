b57a8854128fc12ce51e2663d316852e013fa344
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-08-28 21:40:38 +0200

    Resolved for prod.

--- app/actions/server/broadcast.ts
+++ app/actions/server/broadcast.ts
@@ -1,11 +1,11 @@
-"use server"; // $COMMENT#REMARKS#USECLIENT
+"use server"; // required, among other reasons, to inform React that if this module is to be imported on the client (as it does for WebSocketsClientPage), it needs to be imported as reference
 
 import { broadcastFlow } from "@/actions/server/flows/broadcast";
 
 /**
- * $COMMENT#JSDOC#ACTIONS#DEFS#BROADCASTACTION
- * @param message $COMMENT#JSDOC#ACTIONS#PARAMS#MESSAGE
- * @param displayUsername $COMMENT#JSDOC#ACTIONS#PARAMS#DISPLAYUSERNAME
+ * The Server Function that triggers broadcasts directly from the client, by passing the new message to the server, and the username of its sender if authenticated.
+ * @param message The current latest message sent from the client.
+ * @param displayUsername The username to be displayed sent from the current session. If it exists, this lets the action know which existing user sent the current message. If it doesn't, this lets the action know the current message was sent from a guest (unauthenticated).
  * @returns
  */
 export async function broadcastAction(
--- app/reads/messages.ts
+++ app/reads/messages.ts
@@ -1,6 +1,6 @@
 import { prisma } from "~/prisma/db";
 
-/** $COMMENT#JSDOC#READS#DEFS#FINDLATESTMESSAGES */
+/** Finds the latest messages from the Prisma database. At this time, transforms them directly into an array of objects containing their message, the message's ID, and the username of the user who sent it (with `null` instead if no such user is authenticated). The array is then reversed to display the messages in the correct visual order. */
 export async function findLatestMessages() {
   return (
     await prisma.message.findMany({
@@ -27,7 +27,7 @@ export async function findLatestMessages() {
     .reverse();
 }
 
-/** $COMMENT#JSDOC#READS#DEFS#COUNTALLMESSAGES */
+/** Counts all the messages saved in the database, in order to ascertain its persistence between reboots and deployments. */
 export async function countAllMessages() {
   return await prisma.message.count();
 }
